f457998cc651c0edb072c1e62a0107bdcb167dd7
root <angu123angu@gmail.com>
2020-07-11 10:55:03 -0400

    Second Commit

--- project1.py
+++ project1.py
@@ -1,3 +1,4 @@
+import os, time
 from zipfile import ZipFile
 zip_file_name = "samples.zip"
 filename,file_extension = os.path.splitext(zip_file_name)
@@ -7,4 +8,15 @@ with ZipFile(zip_file_name, 'r') as zip:
     print('Extracting all the files now...') 
     zip.extractall() 
     print('Done!')
+    files = os.listdir(filename)
+    for i in files:
+            file_name = os.path.basename(i)
+            full_path = os.path.abspath(file_name)
+            dir_path = os.path.dirname(os.path.abspath(file_name))
+            mod_time = os.path.getctime(dir_path+"/"+filename+"/"+file_name)
+            mod_time1 = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(mod_time))
+            create_time = os.path.getctime(dir_path+"/"+filename+"/"+file_name)
+            create_time1 = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(create_time))
+            name,extension = os.path.splitext(file_name)
+            print('Name':file_name, 'Full-Path':full_path, 'Last-Modified':mod_time1, 'Data-Created':create_time1, 'File-Extension':no_gap_extension)
 
