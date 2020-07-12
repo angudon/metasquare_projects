f7078aeca7dd0c27acd93ad3cfa301ad698fce75
root <angu123angu@gmail.com>
2020-07-12 09:12:00 -0400

    Second commit

--- project2.py
+++ project2.py
@@ -1,5 +1,17 @@
+from flask import Flask, request, url_for
+
 file_name = 'SalesJan2009.csv'
 num_rows=0
 for rows in open(file_name):
     num_rows+=1
-print(num_rows)
+
+@app.route('/')
+def index():
+    return '''
+        <form method="POST" action="/successful" enctype="multipart/form-data">
+            <h2>Upload your csv File</h2>
+            <input type="file" name="csv_file">
+            <input type="submit">
+            </form>
+    '''
+
