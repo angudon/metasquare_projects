f510f2081e01e7ee1507f4635bc062cf6bc496ea
root <angu123angu@gmail.com>
2020-07-12 09:14:20 -0400

    Third commit

--- project2.py
+++ project2.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, url_for
+from flask_pymongo import PyMongo
 
 file_name = 'SalesJan2009.csv'
 num_rows=0
 for rows in open(file_name):
     num_rows+=1
+
+app = Flask(__name__);
+app.config['MONGO_URL'] = '<mongo_db_url>'
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
@@ -14,4 +19,14 @@ def index():
             <input type="submit">
             </form>
     '''
+
+@app.route('/successful', methods=['POST'])
+def create():
+    if 'csv_file' in request.files:
+        csv_file = request.files['csv_file']
+        mongo.save_file(csv_file.filename, csv_file)
+        mongo.db.users.insert({'username' : request.form.get('username'), 'csv_file_name' : csv_file.filename})
+
+    return 'Uploading completed.\nA total of '+num_rows+' records were inserted into MongoDB.'
+    
 
