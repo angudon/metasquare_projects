c4d024144a783e06d0809e9dbabc5a99eda617a5
root <angu123angu@gmail.com>
2020-07-13 06:45:29 -0400

    Project2 Completed

--- project2.py
+++ project2.py
@@ -1,43 +1,34 @@
-#Modules and tools required:
-    #1)pip3 install flask-pymongo
-    #2)Install mongo tool for creating a local database . Refer to this link https://docs.mongodb.com/manual/tutorial/install-mongodb-on-debian/ for installation.
+from flask import Flask,request, redirect, url_for
 
+from pymongo import MongoClient
+import json
+import pandas as pd
 
-#importing all the modules
-from flask import Flask, request, url_for
-from flask_pymongo import PyMongo
+app = Flask(__name__)
+client = MongoClient('<mongodb-url>')
+db = client['metasquare']
+col = db['project2']
 
-#creating a web server
-app = Flask(__name__);
-
-#intializing the mongo url
-app.config['MONGO_URL'] = '<mongo_db_url>'
-mongo = PyMongo(app)
-
-#creating a homepage for uploading a csv file 
 @app.route('/')
 def index():
     return '''
-        <form method="POST" action="/successful" enctype="multipart/form-data">
-            <h2>Upload your csv File</h2>
-            <input type="file" name="csv_file">
-            <input type="submit">
-            </form>
+        <form method="POST" enctype="multipart/form-data" action="/uploading">
+            <h2>Upload only csv file</h2>
+            <input type="file" name="document_file">
+            <input type="submit" >
     '''
-
-#Processing the uploaded csv file 
-@app.route('/successful', methods=['POST'])
-def create():
-    if 'csv_file' in request.files:
-        csv_file = request.files['csv_file']
-        mongo.save_file(csv_file.filename, csv_file)
-        mongo.db.users.insert({'username' : request.form.get('username'), 'csv_file_name' : csv_file.filename})
- #      num_rows = mongo.db.csv_file.find().count()
- #   return 'Uploading completed.\nA total of '+num_rows+' records were inserted into MongoDB.'
-    return 'Uploading completed.' 
+@app.route('/uploading',methods=['POST'])
+def uploading():
+    if 'document_file' in request.files:
+        document_file = request.files['document_file']
+        df = pd.read_csv(document_file)
+        data = df.to_dict('records')
+        count = len(data)
+        col.insert_many(data, ordered = False)
+        return 'Uploading completed.\nA total of '+str(count)+' records were inserted into MongoDB.'
 
 
- #In terminal:
-    #3)mongo <db-name>
-    #4)show dbs 
-    #5)use <db-name>
+#Compiling the program:
+    #1)export FLASK_APP=project2.py
+    #2)export FLASK_DEBUG=1
+    #3)flask run
